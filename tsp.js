621802eb8678726dbc9093dfa676d9b772534b2a
gueyemoo <m.gueye0411@gmail.com>
2021-12-11 17:07:36 +0100

    refactor

--- tsp.js
+++ tsp.js
@@ -3,11 +3,11 @@ let MENUSCREEN = 0;
 let STARTSCREEN = 1;
 let screenState = MENUSCREEN;
 
-//Application Parameters//
+//Application Parameters: Change those values as you wish to test the algorithm//
 let numberOfCountries = 10;
-let mutationRateValue = 0.01;
+let mutationRateValue = 0.01; //100% = 1 and 1% = 0.01
 let popSize = 50;
-//---------------------//
+//-----------------------------------------------------------------------------//
 
 let countries = [];
 
@@ -27,14 +27,15 @@ let fitness = [];
 
 let savedDistance = [];
 
+//Default function of p5.js that initialise everything
 function setup() {
   //setting up the full screen
   let myCanvas = createCanvas(windowWidth / 2, windowHeight);
-  myCanvas.parent("whole_algorithm");
+  myCanvas.parent("algorithm_container");
 
   for (let i = 0; i < numberOfCountries; i++) { //we create the countries and an array of order
-    let randomX = [150,250, 83, 182, 507,120,400,133,222,712];
-    let randomY = [123, 333, 287, 210, 180,300,200,320,272,222];
+    let randomX = [150, 250, 83, 182, 507, 120, 400, 133, 222, 712];
+    let randomY = [123, 333, 287, 210, 180, 300, 200, 320, 272, 222];
     countries[i] = createVector(randomX[i], randomY[i]); //For test and comparaison purpose
     // countries[i] = createVector(random(width), randomIntFromInterval(100,350));
     order[i] = i;
@@ -48,7 +49,7 @@ function setup() {
   }
 }
 
-
+//Default function of p5.js drawing on screen 
 function draw() {
   background(0);
   if (screenState == MENUSCREEN) {
@@ -58,12 +59,13 @@ function draw() {
     // background(0);
     fill(255);
     textSize(24);
-    text(`${round(millis() / 1000)} seconds have gone by`, width/2, height / 2);
+    text(`${round(millis() / 1000)} seconds have gone by`, width / 2, height / 2);
   } else {
     print("Error!");
   }
 }
 
+//Draw the menu screen
 function drawMenu() {
 
   // Text title
@@ -76,9 +78,10 @@ function drawMenu() {
   text("Press any key to start", (windowWidth / 4), 85);
 }
 
+//Draw the algorithm screen
 function drawStart() {
 
-  //Genectics algorithm function
+  //Genectics algorithm function call
   calculateFitness();
   pourcentFitness();
   nextGeneration();
@@ -120,20 +123,8 @@ function cross(arr, a, b) {
   arr[b] = temporary;
 }
 
-function crossOver(orderA, orderB) {
-  let arrStart = floor(random(orderA.length));
-  let arrEnd = floor(random(arrStart + 1, orderA.length));
-  let neworder = orderA.slice(arrStart, arrEnd);
-  for (let i = 0; i < orderB.length; i++) {
-    let country = orderB[i];
-    if (!neworder.includes(country)) {
-      neworder.push(country);
-    }
-  }
-  return neworder;
-}
-
-function calculateDistance(nodes) { //return the total distance of a path by making the sum between node i and node i+1 (not optimised)
+//return the total distance of a path by making the sum between node i and node i+1 (not optimised because of dist using root²)
+function calculateDistance(nodes) {
   let total = 0;
   for (let i = 0; i < nodes.length - 1; i++) {
     let d = dist(nodes[i].x, nodes[i].y, nodes[i + 1].x, nodes[i + 1].y);
@@ -142,7 +133,8 @@ function calculateDistance(nodes) { //return the total distance of a path by mak
   return total;
 }
 
-function calculateDistanceWithPop(nodes, order) { //Basically a calcultateDistance but more optimised with the use of the population order
+//Basically a calcultateDistance but more optimised with the use of the population order
+function calculateDistanceWithPop(nodes, order) {
   let total = 0;
   for (let i = 0; i < order.length - 1; i++) {
     let first_countrieIndex = order[i];
@@ -155,21 +147,26 @@ function calculateDistanceWithPop(nodes, order) { //Basically a calcultateDistan
   return total;
 }
 
+//Change screen state on key pressed
 function keyPressed() {
   screenState = STARTSCREEN;
 }
 
-function randomIntFromInterval(min, max) { // generate a random number between two values 
+//Generate a random number between two values 
+function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function generateRandomHexColor() { // generate a random hexa color
+//Generate a random hexa color (not used anymore in the code)
+function generateRandomHexColor() {
   return `#${Math.random().toString(16).substring(2, 8)}`;
 }
 
-//Genetics Algorithms
 
-function calculateFitness() { //Calculate the fitness value of a distance
+//Genetics Algorithms//
+
+//Calculate the fitness value of a distance and associate it to the distance
+function calculateFitness() {
   let currentRecord = Infinity;
 
   for (let i = 0; i < population.length; i++) {
@@ -180,11 +177,9 @@ function calculateFitness() { //Calculate the fitness value of a distance
       savedDistance.push(recordDistance.toFixed(2));
       finalBestDistance = population[i];
       time = round(millis() / 1000);
-      if(!timeSaved.includes(time)){
+      if (!timeSaved.includes(time)) {
         timeSaved.push(time);
       }
-      // console.log("record : " + recordDistance);
-      // console.log("larray de save : " + savedDistance);
     }
 
     if (distance < currentRecord) {
@@ -196,7 +191,8 @@ function calculateFitness() { //Calculate the fitness value of a distance
   }
 }
 
-function pourcentFitness() { //this function help us to set the fitness between 0 to 100% (all of them together being 100%)
+//This function help us to set the fitness in pourcentage between 0 to 100% (all of them together being 100%)
+function pourcentFitness() {
   let total = 0;
 
   for (let i = 0; i < fitness.length; i++) {
@@ -213,7 +209,8 @@ function pourcentFitness() { //this function help us to set the fitness between 
   }
 }
 
-function selectOne(arr, fitness) { //select an order according to his fitness probability value
+//Select an order according to his fitness probability value
+function selectOne(arr, fitness) {
   let index = 0;
   let randomNum = random(1);
 
@@ -225,7 +222,22 @@ function selectOne(arr, fitness) { //select an order according to his fitness pr
   return arr[index].slice();
 }
 
-function nextGeneration() { //create the next generation 
+//Make the cross over between to different order A and B
+function crossOver(orderA, orderB) {
+  let arrStart = floor(random(orderA.length));
+  let arrEnd = floor(random(arrStart + 1, orderA.length));
+  let neworder = orderA.slice(arrStart, arrEnd);
+  for (let i = 0; i < orderB.length; i++) {
+    let country = orderB[i];
+    if (!neworder.includes(country)) {
+      neworder.push(country);
+    }
+  }
+  return neworder;
+}
+
+//Create the next generation using select, crossover and mutation
+function nextGeneration() {
   let newGeneration = [];
   for (let i = 0; i < population.length; i++) { //for every member of the existing population we make a new member of the new population 
     // let order = selectOne(population, fitness);
@@ -238,13 +250,14 @@ function nextGeneration() { //create the next generation
   population = newGeneration;
 }
 
-function mutation(order, mutationRate) { //Make change on the best population
+//Make change of index and his neighbors on a given order
+function mutation(order, mutationRate) {
   for (let i = 0; i < numberOfCountries; i++) {
-    if(random(1) < mutationRate) {
+    if (random(1) < mutationRate) {
       let indexA = floor(random(order.length));
       let indexB = (indexA + 1) % numberOfCountries;
       cross(order, indexA, indexB);
     }
-    
+
   }
 }
